98ee8f4299b68639a9e922f8db356303875a085c
Blake Gotting <brgotting@gmail.com>
2018-10-19 16:20:38 -0700

    Change classes of laguages back to IDS.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $("form").submit(function(event) {
+  $("form.quiz").submit(function(event) {
     var companySize = parseInt($("select#companySize").val());
     var companyComplexity = parseInt($("select#companyComplexity").val());
     var companyPreference = parseInt($("select#companyPreference").val());
@@ -8,11 +8,11 @@ $(document).ready(function() {
 
 
     if ("#companySize" + "#companyComplexity" + "#companyPreference" === 3) {
-      $('rubyOnRails').show();
+      $('#rubyOnRails').show();
     } else if("#companySize" + "#companyComplexity" + "#companyPreference" > 3 <6) {
-      $('cSharp').show();
+      $('#cSharp').show();
     } else {("#companySize" + "#companyComplexity" + "#companyPreference" >= 6)
-      $('java').show();
+      $('#java').show();
     }
     event.preventDefault();
   });
