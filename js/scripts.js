3ab700f5132b2aebe37d65c543f578230c6ad553
Blake Gotting <brgotting@gmail.com>
2018-10-19 12:30:03 -0700

    Add if statement in javascript.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
   $("form#quiz").submit(function(event) {
-    var size = parseInt($("option#companySize").val());
+    var size = parseInt($("input#companySize").val());
     var complexity = parseInt($("option#companyComplexity").val());
     var companyPreference = parseInt($("option#companyPreference").val());
+    event.preventDefault();
+
+    if (companySize + companyComplexity + companyPreference === 3) {
+      $('rubyOnRails').show();
+    }
   }
+
+
 
 });
