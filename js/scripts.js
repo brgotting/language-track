b993ee53efaf337289d1f001448d5eea975c05b7
Blake Gotting <brgotting@gmail.com>
2018-10-19 14:03:54 -0700

    Continue looking for a fix, submit button still not working.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
-  $("#quiz").submit(function(event) {
+  $("form").submit(function(event) {
     var companySize = parseInt($("option#companySize").val());
     var companyComplexity = parseInt($("option#companyComplexity").val());
     var companyPreference = parseInt($("option#companyPreference").val());
      $("#output").text(result);
 
 
-    if ("companySize" + "companyComplexity" + "companyPreference" === 3) {
-      $('#rubyOnRails').show();
-    } else if("companySize" + "companyComplexity" + "companyPreference" > 3) {
+    if ("#companySize" + "#companyComplexity" + "#companyPreference" === 3) {
+      $(".rubyOnRails").show();
+    } else if("#companySize" + "#companyComplexity" + "#companyPreference" > 3) {
       $('#cSharp').show();
-    } else("companySize" + "companyComplexity" + "companyPreference" >= 6) {
+    } else {("#companySize" + "#companyComplexity" + "#companyPreference" >= 6)
       $('#java').show();
     }
     event.preventDefault();
