757d95ef2c2258debdd8433cff7bdf88635b81ac
Blake Gotting <brgotting@gmail.com>
2018-10-19 11:44:19 -0700

    Add javascript fn, define variables.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
-  $
+  $("form#quiz").submit(function(event) {
+    var size = parseInt($("option#companySize").val());
+    var complexity = parseInt($("option#companyComplexity").val());
+    var companyPreference = parseInt($("option#companyPreference").val());
+  }
 
 });
