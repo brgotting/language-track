e88aae56a30e84247402392a676d5eb7564c32b7
Blake Gotting <brgotting@gmail.com>
2018-10-19 14:31:12 -0700

    Add type=submit, did not work.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,18 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
-    var companySize = parseInt($("option#companySize").val());
-    var companyComplexity = parseInt($("option#companyComplexity").val());
-    var companyPreference = parseInt($("option#companyPreference").val());
-     $("#output").text(result);
+    var companySize = parseInt($("select#companySize").val());
+    var companyComplexity = parseInt($("select#companyComplexity").val());
+    var companyPreference = parseInt($("select#companyPreference").val());
+
+
 
 
     if ("#companySize" + "#companyComplexity" + "#companyPreference" === 3) {
-      $(".rubyOnRails").show();
+      $('rubyOnRails').show();
     } else if("#companySize" + "#companyComplexity" + "#companyPreference" > 3) {
-      $('#cSharp').show();
+      $('cSharp').show();
     } else {("#companySize" + "#companyComplexity" + "#companyPreference" >= 6)
-      $('#java').show();
+      $('java').show();
     }
     event.preventDefault();
   });
